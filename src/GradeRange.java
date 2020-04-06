92d40e0658dc11ec0049765e60a738612bde00c3
Er1c02 <majesticr11@gmail.com>
2020-04-06 11:23:03 -0700

    Initial commit

---
+++ src/GradeRange.java
@@ -0,0 +1,12 @@
+public class GradeRange
+{
+    public static void main (String[] args)
+    {
+        String[] grades = {"A","A-","B+","B","B-","C+","C","C",
+                "D+","D","D-","F"};
+        int[] cutoff = {95,90,87,83,89,77,73,70,67,63,60,0};
+        for (int level = 0; level < cutoff.length; level++)
+            System.out.println (grades[level] + "\t" + cutoff[level]);
+    }//End main method
+
+}//End class GradeRange
